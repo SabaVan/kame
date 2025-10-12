913015dfe7b0474664a869234d5309e04f9075b1
Gabrielius Butkus <gabrielius.butkus@mif.stud.vu.lt>
2025-10-12 19:23:38 +0300

    style-13: fix formatting in Song and PlaylistSong

--- backend/Models/PlaylistSong.cs
+++ backend/Models/PlaylistSong.cs
@@ -1,49 +1,49 @@
 namespace backend.Models
 {
-  public class PlaylistSong : IComparable<PlaylistSong>
-  {
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public required Guid PlaylistId { get; set; }
-    public required Guid SongId { get; set; }
-    public required Song Song { get; set; }
-    public required Guid AddedByUserId { get; set; }
-    public DateTime AddedAt { get; set; } = DateTime.Now;
-    public int CurrentBid { get; set; } // Highest bid
-    public Guid? CurrentBidderId { get; set; }
-    public int Position { get; set; }
-
-    public void AddBid(int amount)
+    public class PlaylistSong : IComparable<PlaylistSong>
     {
-      if (amount <= 0)
-      {
-        throw new ArgumentException("Bid amount must be positive", nameof(amount));
-      }
-      if (amount <= CurrentBid)
-      {
-        throw new ArgumentException("Bid amount must be higher than current bid", nameof(amount));
-      }
-      CurrentBid = amount;
-    }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required Guid PlaylistId { get; set; }
+        public required Guid SongId { get; set; }
+        public required Song Song { get; set; }
+        public required Guid AddedByUserId { get; set; }
+        public DateTime AddedAt { get; set; } = DateTime.Now;
+        public int CurrentBid { get; set; } // Highest bid
+        public Guid? CurrentBidderId { get; set; }
+        public int Position { get; set; }
 
-    public int CompareTo(PlaylistSong? other)
-    {
-      if (other == null) return 1;
-
-      int bidComparison = other.CurrentBid.CompareTo(CurrentBid);
-
-      if (bidComparison != 0)
-      {
-        return bidComparison;
-      }
-      else
-      {
-        int addedAtComparison = other.AddedAt.CompareTo(AddedAt);
-        if (addedAtComparison != 0)
+        public void AddBid(int amount)
         {
-          return addedAtComparison;
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Bid amount must be positive", nameof(amount));
+            }
+            if (amount <= CurrentBid)
+            {
+                throw new ArgumentException("Bid amount must be higher than current bid", nameof(amount));
+            }
+            CurrentBid = amount;
         }
-        return 0;
-      }
+
+        public int CompareTo(PlaylistSong? other)
+        {
+            if (other == null) return 1;
+
+            int bidComparison = other.CurrentBid.CompareTo(CurrentBid);
+
+            if (bidComparison != 0)
+            {
+                return bidComparison;
+            }
+            else
+            {
+                int addedAtComparison = other.AddedAt.CompareTo(AddedAt);
+                if (addedAtComparison != 0)
+                {
+                    return addedAtComparison;
+                }
+                return 0;
+            }
+        }
     }
-  }
 }
