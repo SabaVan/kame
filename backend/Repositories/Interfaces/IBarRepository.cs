6a72e206c671f059341c091117bfd3820221a437
saba <erwinlody@gmail.com>
2025-10-12 22:36:35 +0300

    feat: Created IBarRepository

---
+++ backend/Repositories/Interfaces/IBarRepository.cs
@@ -0,0 +1,12 @@
+using backend.Models;
+namespace backend.Repositories.Interfaces
+{
+    public interface IBarRepository
+    {
+        Task<List<Bar>> GetAllAsync();
+        Task<Bar?> GetByIdAsync(Guid id);
+        Task AddAsync(Bar bar);
+        Task<Bar?> UpdateAsync(Bar bar);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
