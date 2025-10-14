88a00c13fd4b8b35621b6c06bb13d60077b048a4
saba <erwinlody@gmail.com>
2025-10-14 16:39:27 +0300

    style: removed comment

--- backend/Utils/Result.cs
+++ backend/Utils/Result.cs
@@ -8,11 +8,3 @@ namespace backend.Utils
         public static Result<T> Failure(Error err) => new(false, default, err);
     }
 }
-/* For example
-Result<int> success = Result<int>.Success(42);
-Console.WriteLine(success.IsSuccess); // True
-
-Result<int> failure = Result<int>.Failure("NOT_FOUND", "Item not found");
-Console.WriteLine(failure.Value);     // null
-Console.WriteLine(failure.IsFailure); // True
-Console.WriteLine(failure.Error?.Message); // "Item not found" */
