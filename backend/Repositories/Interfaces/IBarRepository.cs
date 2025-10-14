58b34dbb7f3945a8c714c3ea5100c8ed1f91ddf8
saba <erwinlody@gmail.com>
2025-10-14 17:57:23 +0300

    refactor: remove GetAllAsync and switch to using database

--- backend/Repositories/Interfaces/IBarRepository.cs
+++ backend/Repositories/Interfaces/IBarRepository.cs
@@ -3,10 +3,10 @@ namespace backend.Repositories.Interfaces
 {
     public interface IBarRepository
     {
-        Task<List<Bar>> GetAllAsync();
         Task<Bar?> GetByIdAsync(Guid id);
         Task AddAsync(Bar bar);
         Task<Bar?> UpdateAsync(Bar bar);
         Task<bool> DeleteAsync(Guid id);
+        Task SaveChangesAsync();
     }
 }
