97075a5578fb51de074aa7f94682ac75190516c0
saba <erwinlody@gmail.com>
2025-10-14 22:31:33 +0300

    feat:  GetAllAsync

--- backend/Repositories/Interfaces/IBarRepository.cs
+++ backend/Repositories/Interfaces/IBarRepository.cs
@@ -3,6 +3,7 @@ namespace backend.Repositories.Interfaces
 {
     public interface IBarRepository
     {
+        Task<List<Bar>> GetAllAsync();
         Task<Bar?> GetByIdAsync(Guid id);
         Task AddAsync(Bar bar);
         Task<Bar?> UpdateAsync(Bar bar);
