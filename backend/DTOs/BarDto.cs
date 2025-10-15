36f002dd5418cf27ffe69136e85c3ead1e7facd3
saba <erwinlody@gmail.com>
2025-10-15 12:04:48 +0300

    feat: BarDto represents fields data types after mapping, before sending to frontend

---
+++ backend/DTOs/BarDto.cs
@@ -0,0 +1,11 @@
+namespace backend.DTOs
+{
+    public class BarDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;  // "Open", "Closed", etc.
+        public string OpenAt { get; set; } = string.Empty; // formatted string
+        public string CloseAt { get; set; } = string.Empty; // formatted string
+    }
+}
