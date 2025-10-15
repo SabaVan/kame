8d22eeff5045efc5df60c441aa9bcbbb53551f8d
saba <erwinlody@gmail.com>
2025-10-15 12:07:49 +0300

    fix: temporarily commented unused line

--- backend/Services/SimpleBarService.cs
+++ backend/Services/SimpleBarService.cs
@@ -4,12 +4,13 @@ using System.Threading.Tasks;
 using backend.Models;
 using backend.Repositories.Interfaces;
 using backend.Repositories;
+using backend.Services.Interfaces;
 
 namespace backend.Services
 {
-    public class SimpleBarService
+    public class SimpleBarService : IBarService
     {
-        private readonly IBarRepository _bars;
+        // private readonly IBarRepository _bars;
         // private readonly IUserRepository _users;
         // private readonly IPlaylistRepository _playlistService;
         // private readonly ICreditManager _credits;
