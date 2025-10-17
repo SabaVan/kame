1243f7d156d331e7241eaa520dfd679f07179b8e
saba <erwinlody@gmail.com>
2025-10-17 17:50:15 +0300

    feat: CheckSchedule, EnterBar, LeaveBar - integrate BarUserEntryRepository

--- backend/Services/SimpleBarService.cs
+++ backend/Services/SimpleBarService.cs
@@ -1,23 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using backend.Models;
 using backend.Repositories.Interfaces;
-using backend.Repositories;
 using backend.Utils.Errors;
 using backend.Services.Interfaces;
-using backend.Utils;
+using backend.Common;
 using backend.Enums;
-
 namespace backend.Services
 {
     public class SimpleBarService : IBarService
     {
         private readonly IBarRepository _bars;
+        private readonly IBarUserEntryRepository _barUserEntries;
         // private readonly IUserRepository _users;
         // private readonly IPlaylistRepository _playlistService;
         // private readonly ICreditManager _credits;
-        public SimpleBarService(IBarRepository bars)
+        public SimpleBarService(IBarRepository bars, IBarUserEntryRepository barUserEntries)
         {
             ArgumentNullException.ThrowIfNull(bars);
             //ArgumentNullException.ThrowIfNull(users);
@@ -25,6 +21,7 @@ namespace backend.Services
             //ArgumentNullException.ThrowIfNull(credits);
 
             _bars = bars;
+            _barUserEntries = barUserEntries;
             //_users = users;
             //_playlistService = playlistService;
             //_credits = credits;
@@ -41,6 +38,34 @@ namespace backend.Services
             bar.SetState(newState);
             await _bars.SaveChangesAsync();
             return Result<Bar?>.Success(bar);
+        }
+
+        public async Task<Result<BarUserEntry>> EnterBar(Bar bar, User user)
+        {
+            BarUserEntry entry = new BarUserEntry(bar, user);
+            await _barUserEntries.AddEntryAsync(entry);
+            return Result<BarUserEntry>.Success(entry);
+        }
+        public async Task<Result<BarUserEntry>> LeaveBar(Bar bar, User user)
+        {
+            var entryResult = await _barUserEntries.FindEntryAsync(bar.Id, user.Id);
+            if (entryResult.IsFailure)
+                return Result<BarUserEntry>.Failure(StandardErrors.NonexistentEntity);
+            var entry = entryResult.Value;
+            await _barUserEntries.RemoveEntryAsync(entry!);
+            return entryResult;
+        }
+        public async Task CheckSchedule(DateTime nowUtc)
+        {
+            var allBars = await _bars.GetAllAsync();
+            foreach (var bar in allBars)
+            {
+                if (bar.ShouldBeOpen(nowUtc))
+                {
+                    if (bar.State == BarState.Closed) bar.SetState(BarState.Open);
+                }
+                else if (bar.State == BarState.Open) bar.SetState(BarState.Closed);
+            }
         }
     }
 }
