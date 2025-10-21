21c000be262104821809fd0e9e7066737cc92bbb
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2025-10-21 23:01:15 +0300

    feat: create TransactionRepo and interface for it

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -13,6 +13,7 @@ namespace backend.Data
         public DbSet<User> Users { get; set; }
         //public DbSet<Song> Songs { get; set; }
         //public DbSet<PlayList> PlayLists { get; set; }
+        public DbSet<CreditTransaction> CreditTransactions { get; set; } // maps user to it's credit transactions
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
