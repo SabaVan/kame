91b153bd10d62a2d18cde744198a8ca501480560
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2025-10-21 22:52:30 +0300

    fix: fix CreditTransaction - create automatically the guid, timestamp

--- backend/Models/CreditTransaction.cs
+++ backend/Models/CreditTransaction.cs
@@ -2,12 +2,14 @@ using backend.Shared.Enums;
 
 namespace backend.Models
 {
-    public record CreditTransaction(
-        Guid Id,
-        Guid UserId,
-        int Amount,
-        string Reason,
-        DateTime CreatedAt,
-        TransactionType Type
-    );
+    public record CreditTransaction
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public Guid UserId { get; init; }
+        public int Amount { get; init; }
+        public required string Reason { get; init; }
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+        public TransactionType Type { get; init; }
+    }
+
 }
