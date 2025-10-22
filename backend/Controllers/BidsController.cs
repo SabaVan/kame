17e0bf5554a930769e04e36898f430af5d984c8a
Gabrielius Butkus <gabrielius.butkus@mif.stud.vu.lt>
2025-10-22 11:50:15 +0300

    feat: Add BidsController

---
+++ backend/Controllers/BidsController.cs
@@ -0,0 +1,25 @@
+using backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class BidsController : ControllerBase
+  {
+    private readonly IPlaylistService _playlistService;
+
+    public BidsController(IPlaylistService playlistService)
+    {
+      _playlistService = playlistService;
+    }
+
+    [HttpPost("place")]
+    public async Task<IActionResult> PlaceBid([FromQuery] Guid userId, [FromQuery] Guid songId, [FromQuery] int amount)
+    {
+      var result = await _playlistService.BidOnSongAsync(userId, songId, amount);
+      if (!result.IsSuccess) return BadRequest(result.Error?.Message);
+      return Ok(result.Value);
+    }
+  }
+}
