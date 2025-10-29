567e7ba927fa6ba2a607328e431b1d7f9cee1318
Gabrielius Butkus <gabrielius.butkus@mif.stud.vu.lt>
2025-10-29 17:40:45 +0200

    refactor: Change to struct

--- backend/Shared/DTOs/SongDto.cs
+++ backend/Shared/DTOs/SongDto.cs
@@ -1,12 +1,13 @@
 namespace backend.Shared.DTOs
 {
-    public class SongDto
+
+    public struct SongDto
     {
         public Guid Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Artist { get; set; } = string.Empty;
+        public string Title { get; set; }
+        public string Artist { get; set; }
         public int Votes { get; set; }
-        public int CurrentBid { get; set; } = 0;
+        public int CurrentBid { get; set; }
 
     }
 }
