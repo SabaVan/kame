8c1a85c3f576a619ccbd9aeb0f6d7c4c18ce92ec
saba <erwinlody@gmail.com>
2025-10-29 08:17:35 +0200

    update migrations

--- backend/Migrations/20251028185450_InitialCreate.Designer.cs
+++ backend/Migrations/20251028185450_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using backend.Data;
 namespace backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251028181853_LatestChange")]
-    partial class LatestChange
+    [Migration("20251028185450_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
