0abb5daead4b68991ae5660d37cde3855ca4e6ac
Gabrielius Butkus <gabrielius.butkus@mif.stud.vu.lt>
2025-10-29 17:39:53 +0200

    refactor: Add initial credits

--- backend/Models/Credits.cs
+++ backend/Models/Credits.cs
@@ -1,27 +1,27 @@
 namespace backend.Models
 {
-    public class Credits
+  public class Credits
+  {
+    public int Total { get; set; }
+    protected Credits() { }
+    public Credits(int initialAmount = 100)
     {
-        public int Total { get; set; }
-        protected Credits() { }
-        public Credits(int initialAmount = 0)
-        {
-            Total = initialAmount;
-        }
-
-        public bool TrySpend(int amount)
-        {
-            if (amount <= Total)
-            {
-                Total -= amount;
-                return true;
-            }
-            return false;
-        }
-
-        public void Add(int amount)
-        {
-            Total += amount;
-        }
+      Total = initialAmount;
     }
+
+    public bool TrySpend(int amount)
+    {
+      if (amount <= Total)
+      {
+        Total -= amount;
+        return true;
+      }
+      return false;
+    }
+
+    public void Add(int amount)
+    {
+      Total += amount;
+    }
+  }
 }
